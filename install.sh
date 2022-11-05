fabc68d4a24c1892d676f600977a7ce0e05fdef0
Deliander <72639023+Deliander@users.noreply.github.com>
2022-11-05 14:21:52 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apk update        # Update
-apt add autoremove         # Auto Remove
+apk add autoremove         # Auto Remove
 
 apk add py3-pip # Installing Pip
 echo installed pip         # Telling User that installing is done
