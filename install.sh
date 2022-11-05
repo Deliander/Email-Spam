70d2eb4d7ddc16ba67fd2f042684a9830c9c2e31
Deliander <72639023+Deliander@users.noreply.github.com>
2022-11-05 14:14:30 +1100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+apk update        # Update
+apt add autoremove         # Auto Remove
+
+apk add py3-pip # Installing Pip
+echo installed pip         # Telling User that installing is done
+
+apk add pysocks        # Installing pysocks
+echo "installed pysocks"   # Teling User that installing is done
+
+chmod +x Email-Spam.py
